db39a74c967c71f8ac4294aa2785463eacd3b47b
plana <13058160568@163.com>
2025-05-30 17:31:27 +0800

    init project

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SunSet.Core
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("??");
+        }
+    }
+}
